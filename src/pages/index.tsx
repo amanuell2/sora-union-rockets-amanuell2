9fa1fbf2fa13dd25439657a78425fcaf03bad83a
amanuel <amanuel0000@gmail.com>
2023-04-04 13:46:45 +0300

    bind and update user review with form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,16 +9,49 @@ import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { useState } from "react";
-import { FaEdit } from "react-icons/fa";
-import { FaTrash } from "react-icons/fa";
+import React, { useEffect } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import { z } from "zod";
+import { type SubmitHandler, useForm, } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RocketContextProvider, useRocket } from "~/pages/rocket.context";
+import { AutoCompleteInput } from "~/components/AutoCompleteInout";
 
 dayjs.extend(relativeTime);
+type RocketWithUser = RouterOutputs["rockets"]["getAll"][number];
+
+
 
 const CreateRocketWizard = () => {
-  const [rocketTitle, setRocketTitle] = useState<string>("");
+  const { isUpdating, rocket, setIsUpdating } = useRocket();
 
-  const [rocketDescription, setRocketDescription] = useState<string>("");
+  const formSchema = z.object({
+    title: z.string().min(3).max(50),
+    description: z.string().min(3).max(500),
+  })
+
+  type RocketFormType = z.infer<typeof formSchema>;
+
+  const { register, handleSubmit, setValue, reset, formState: { errors, isSubmitting } } = useForm<RocketFormType>({
+    resolver: zodResolver(formSchema)
+  })
+
+  const onSubmit: SubmitHandler<RocketFormType> = (data, e) => {
+    e?.preventDefault()
+    if (isUpdating && rocket?.id) {
+      updateMutate({ id: rocket?.id, ...data })
+      return
+    }
+    mutate(data)
+  }
+
+  useEffect(() => {
+    if (isUpdating && rocket) {
+      setValue("title", rocket?.title, { shouldValidate: true })
+      setValue("description", rocket?.description, { shouldValidate: true })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isUpdating, rocket])
 
   const { user } = useUser();
 
@@ -26,8 +59,26 @@ const CreateRocketWizard = () => {
 
   const { mutate, isLoading: isRocketing } = api.rockets.create.useMutation({
     onSuccess: () => {
-      setRocketDescription("");
-      setRocketTitle("");
+      reset()
+      void ctx.rockets.getAll.invalidate()
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors;
+      // toast error message if it is from the server
+      if (errorMessage) {
+        toast.error(" Please  check your form inputs  ")
+      }
+      else {
+        toast.error("Something went wrong")
+      }
+    }
+  });
+
+
+  const { mutate: updateMutate, isLoading: isReviewUpdating } = api.rockets.update.useMutation({
+    onSuccess: () => {
+      reset()
+      setIsUpdating(false)
       void ctx.rockets.getAll.invalidate()
     },
     onError: (e) => {
@@ -53,65 +104,53 @@ const CreateRocketWizard = () => {
         placeholder="blur"
         blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAA"
       />
-      <div className="flex flex-col w-full">
-        <input type="text" placeholder="rocket title" className="bg-transparent grow outline-none"
-          value={rocketTitle}
-          onChange={(e) => setRocketTitle(e.target.value)}
-          disabled={isRocketing}
-        />
-        <input type="text" placeholder="rocket description" className="bg-transparent grow outline-none border w-11/12 border-rose-100 px-2"
-          value={rocketDescription}
-          onChange={(e) => setRocketDescription(e.target.value)}
-          disabled={isRocketing}
-        />
-      </div>
-      {
-        rocketTitle != "" && !isRocketing && (
-          <button className="bg-rose-100 text-rose-500 rounded-full p-2 w-32"
-            onClick={() => {
-              mutate({
-                title: rocketTitle,
-                description: rocketDescription,
-              })
-            }
-            }
-          >Add</button>
-        )
-      }
-      {
-        isRocketing && (
-          <div className="flex items-center justify-center">
-            <LoadingSpinner size={40} />
-          </div>
-        )
-      }
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-row w-full" >
+        <div className="flex flex-col w-full gap-3">
+          <input type="text" placeholder="rocket title" className="bg-transparent grow outline-none"
+            disabled={isRocketing}
+            {...register("title")}
+          />
+          {errors.title && (
+            <span className="text-red-800 block mt-2">
+              {errors.title?.message}
+            </span>
+          )}
+          <input type="text" placeholder="rocket description" className="bg-transparent grow outline-none border w-9/12 border-rose-100 px-2"
+            {...register("description")}
+          />
+          {errors.description && (
+            <span className="text-red-800 block mt-2">
+              {errors.description?.message}
+            </span>
+          )}
+        <div className="border-rose-100 w-9/12">
+        <AutoCompleteInput/>
+        </div>
+        </div>
+        {
+          !isSubmitting && !isRocketing && !isReviewUpdating && (
+            <button className="bg-rose-100 text-rose-500 self-center w-32 h-32"
+              type="submit"
+            >
+              {isUpdating ? "Update" : "Create"}
+            </button>
+          )}
+        {
+          isRocketing || isReviewUpdating && (
+            <div className="flex items-center justify-center">
+              <LoadingSpinner size={40} />
+            </div>
+          )}
+      </form>
     </div>
   )
-
 }
-
-type RocketWithUser = RouterOutputs["rockets"]["getAll"][number];
 
 const RocketView = (props: RocketWithUser) => {
   const { rocket, author } = props;
-
-  const { mutate: updateMutate } = api.rockets.update.useMutation({
-    onSuccess: () => {
-      void ctx.rockets.getAll.invalidate()
-    },
-    onError: (e) => {
-      const errorMessage = e.data?.zodError?.fieldErrors;
-      // toast error message if it is from the server
-      if (errorMessage) {
-        toast.error(" Please  check your form inputs  ")
-      }
-      else {
-        toast.error("Something went wrong")
-      }
-    }
-  });
-
   const ctx = api.useContext();
+  const { updateRocket } = useRocket();
+
   const { mutate: deleteMutate } = api.rockets.delete.useMutation({
     onSuccess: () => {
       toast.success("Deleted successfully")
@@ -119,15 +158,10 @@ const RocketView = (props: RocketWithUser) => {
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors;
-      // toast error message if it is from the server
-      if (errorMessage) {
-        toast.error(" Please  check your form inputs  ")
-      }
-      else {
-        toast.error("Something went wrong")
-      }
+      if (errorMessage) toast.error("Something went wrong")
     }
   });
+
   return (
     <div className="border-b border-slate-400 p-4 flex gap-3" key={rocket.id}>
       <Image
@@ -146,14 +180,12 @@ const RocketView = (props: RocketWithUser) => {
       </div>
       <div className="flex flex-col self-end">
         <button className="text-rose-100 p-2"
-          onClick={() => {
-            // bind data to the form
-          }}
+          onClick={() => updateRocket(rocket)}
         >
           <FaEdit />
         </button>
         <button className="text-rose-100 p-2"
-          onClick={() =>  deleteMutate({ id: rocket.id })}
+          onClick={() => deleteMutate({ id: rocket.id })}
         >
           <FaTrash />
         </button>
@@ -198,16 +230,18 @@ const Home: NextPage = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex justify-center h-screen">
-        <div className=" w-full md:max-w-2xl h-full border-x border-slate-400">
-          <div className="flex border-b border-slate-400 p-4">
-            {!isSignedIn && (<div className="flex justify-center">
-              <SignInButton />
+        <RocketContextProvider>
+          <div className=" w-full md:max-w-2xl h-full border-x border-slate-400">
+            <div className="flex border-b border-slate-400 p-4">
+              {!isSignedIn && (<div className="flex justify-center">
+                <SignInButton />
+              </div>
+              )}
+              {!!isSignedIn && <CreateRocketWizard />}
             </div>
-            )}
-            {!!isSignedIn && <CreateRocketWizard />}
+            <Feed />
           </div>
-          <Feed />
-        </div>
+        </RocketContextProvider>
       </main>
     </>
   );
