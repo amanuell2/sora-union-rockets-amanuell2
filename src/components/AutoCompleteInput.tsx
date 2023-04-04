2e01b4c8cbc8db18333024ea529b4eaac3d5835b
amanuel <amanuel0000@gmail.com>
2023-04-04 16:05:32 +0300

    house keeping on inputs typo

--- src/components/AutoCompleteInput.tsx
+++ src/components/AutoCompleteInput.tsx
@@ -59,7 +59,7 @@ export const AutoCompleteInput = () => {
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(person) => person?.name}
+              displayValue={(person:User) => person?.name || ''}
               onChange={debouncedResults}
             />
           </div>
